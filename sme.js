228afdb4694a09a56e1a25a81f4f4d83c362ab5b
qaiik <82181231+qaiik@users.noreply.github.com>
2021-12-03 19:08:05 -0700

    Update sme.js

--- sme.js
+++ sme.js
@@ -1,6 +1,6 @@
 let c = document.querySelector("#rso > div:nth-child(1) > div > div.kp-blk.Wnoohf.OJXvsb > div > div.ifM9O > div > div > div > g-lightbox > div.ynlwjd.VDgVie.qzMpzb.u98ib > div.AU64fe.zsYMMe.TUOsUe > span > div > div.jNB0Ic > canvas");
 let fc = document.createElement("canvas");
-c.getContext("2d")drawImage = function (arguments) {
+c.getContext("2d").drawImage = function (arguments) {
   alert("drawn")
   fc.drawImage.apply(null, arguments);
 }
