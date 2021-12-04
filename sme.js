ec60fe4f2affbf798d94c9fe92d47aac02486fb6
qaiik <82181231+qaiik@users.noreply.github.com>
2021-12-03 19:07:38 -0700

    Update sme.js

--- sme.js
+++ sme.js
@@ -1,11 +1,11 @@
 let c = document.querySelector("#rso > div:nth-child(1) > div > div.kp-blk.Wnoohf.OJXvsb > div > div.ifM9O > div > div > div > g-lightbox > div.ynlwjd.VDgVie.qzMpzb.u98ib > div.AU64fe.zsYMMe.TUOsUe > span > div > div.jNB0Ic > canvas");
 let fc = document.createElement("canvas");
-c.drawImage = function (arguments) {
+c.getContext("2d")drawImage = function (arguments) {
   alert("drawn")
   fc.drawImage.apply(null, arguments);
 }
 
-c.fillRect = function (arguments) {
+c.getContext("2d").fillRect = function (arguments) {
   alert("drawn")
   fc.fillRect.apply(null, arguments);
 }
